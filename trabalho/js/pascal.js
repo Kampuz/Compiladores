e0338b0229ec615aead4f3aec6fba748b8190dec
Kampuz <miguel.moret@unesp.br>
2026-03-10 17:31:15 -0300

    feat: added error handling

--- trabalho/js/pascal.js
+++ trabalho/js/pascal.js
@@ -1,4 +1,4 @@
-const TOKEN_TYPES ={
+const TOKEN_TYPES = {
     'program': 'palavra-reservada-program',
     'procedure': 'palavra-reservada-procedure',
     'var': 'palavra-reservada-var',
@@ -12,7 +12,7 @@ const TOKEN_TYPES ={
     '.': 'ponto-final',
     '(': 'abre-parenteses',
     ')': 'fecha-parenteses',
-    ':=': 'atribucão',
+    ':=': 'atribucao',
     '+': 'operacao-soma',
     '-': 'operacao-subtracao',
     '*': 'operacao-multiplicacao',
@@ -32,6 +32,12 @@ const TOKEN_TYPES ={
     'do': 'palavra-reservada-do',
 }
 
+const MAX_LEN = 10
+
+function isVocabulary(symbol) {
+    return (isDigit(symbol) || isLetter(symbol) || getTokenType(symbol) !== 'inválido');
+}
+
 function isDigit(number) {
     return ((number >= '0') && (number <= '9'));
 }
@@ -49,12 +55,22 @@ function getTokenType(token) {
     return TOKEN_TYPES[token] ?? 'inválido';
 }
 
+function isValidIdentifier(token) {
+    if (!isLetter(token[0])) return false;
+    
+    for (let i = 1; i < token.length; i++) {
+        if (!isLetter(token[i]) && !isDigit(token[i])) return false;
+    }
+
+    return true;
+}
+
 
 function handleComment(input, i, state) {
     let firstChar = input[i];
     let secondChar = input[i + 1];
 
-    if (firstChar=== '{') {
+    if (firstChar === '{') {
         i++;
 
         while (i < input.length && input[i] !== '}') {
@@ -65,7 +81,7 @@ function handleComment(input, i, state) {
             i++;
         }
         return i;
-    } else if (firstChar === '/'  && secondChar === '/') {
+    } else if (firstChar === '/' && secondChar === '/') {
         while (i < input.length && input[i] !== '\n') {
             i++;
         }
@@ -83,23 +99,24 @@ function lexicalAnalise(input) {
         offset: 0
     };
 
-    let output = "lexema | token | linha | col_inicial | col_final<br>";
-    
+    let output = "";
+    let error = "";
+
     for (let i = 0; i < input.length; i++) {
-        
-        if (input[i] === ' ') continue;
-        
-        if (input[i] === '\n') {
+
+        if (input[i] === ' ' || input[i] == '\t') continue;
+
+        else if (input[i] === '\n') {
             state.line++;
             state.offset = i + 1;
             continue;
         }
 
-        if (input[i] === '{' || (input[i] === '/' && input[i + 1] === '/')) {
+        else if (input[i] === '{' || (input[i] === '/' && input[i + 1] === '/')) {
             i = handleComment(input, i, state);
             continue;
         }
-        
+
         let col_inicial = i - state.offset + 1;
         let col_final = col_inicial
         let token = '';
@@ -107,67 +124,73 @@ function lexicalAnalise(input) {
 
         if (isDigit(input[i])) {
             let start = i;
-            while(i < input.length && isDigit(input[i])) {
-                i++;
-            }
+            let float_start = 0;
+            while (i < input.length && isDigit(input[i])) i++;
 
-            if (i < input.length &&
-                input[i] === '.' &&
-                isDigit(input[i + 1])) {
-                
+            if (i < input.length && input[i] === '.' && isDigit(input[i + 1])) {
                 i++;
-                while(i < input.length && isDigit(input[i])) {
-                    i++;
-                }
+                float_start = i
+                while (i < input.length && isDigit(input[i])) i++;
                 tokenType = "nReal";
             } else {
-                tokenType = "nInt";
-            }
-
-            token = input.substring(start, i);
-            i--;
-            col_final = i - state.offset + 1;
-
-        } else if (isLetter(input[i])) {
-            let start = i;
-
-            while (i < input.length && (isLetter(input[i]) || isDigit(input[i]))) {
-                i++;
-            }
-
-            token = input.substring(start, i);
-
-            if (TOKEN_TYPES[token]) {
-                tokenType = TOKEN_TYPES[token];
-            } else {
-                tokenType = "identificador";
-            }
-
-            i--;
-            col_final = i - state.offset + 1;
-
-        } else if (input[i] === ':' && input[i + 1] === '=') {
-            token = ':=';
-            tokenType = TOKEN_TYPES[token];
-            i++;
-            col_final = i - state.offset + 1;
-
-        }  else if ((input[i] === '<' && (input[i + 1] === '>' || input[i + 1] === '=') ||
-                    (input[i] === '>' && input[i + 1] === '='))) {
-            token = `${input[i]}${input[i + 1]}`;
-            tokenType = TOKEN_TYPES[token];
-            i++;
-            col_final = i - state.offset + 1;
-
-        } else {
-            token = input[i];
-            console.log(getTokenType(token))
-            tokenType = getTokenType(token);
-            col_final = i - state.offset + 1;
+            tokenType = "nInt";
         }
 
-        output += `${token}  ${tokenType}  ${state.line}  ${col_inicial}  ${col_final}<br>`;
+        token = input.substring(start, i);
+        i--;
+        col_final = i - state.offset + 1;
+        if ((tokenType === "nReal") && ((i + 1) - float_start) > MAX_LEN) {
+            error += `${token}  numero-real-longo  ${state.line}  ${col_inicial}  ${col_final}<br>`
+        }
+
+    } else if (isLetter(input[i])) {
+        let start = i;
+
+        while (i < input.length && (isLetter(input[i]) || isDigit(input[i]))) i++;
+        
+        token = input.substring(start, i);
+        
+        if (TOKEN_TYPES[token]) {
+            tokenType = TOKEN_TYPES[token];
+        } else {
+            if (isValidIdentifier(token))
+                tokenType = "identificador-válido";
+            else tokenType = "identificador-inválido"
+        }
+        
+        i--;
+        col_final = i - state.offset + 1;
+        if ((i - start) > MAX_LEN) {
+            error += `${token}  identificador-longo  ${state.line}  ${col_inicial}  ${col_final}<br>`
+        }
+        
+    } else if (input[i] === ':' && input[i + 1] === '=') {
+        token = ':=';
+        tokenType = TOKEN_TYPES[token];
+        i++;
+        col_final = i - state.offset + 1;
+
+    } else if ((input[i] === '<' && (input[i + 1] === '>' || input[i + 1] === '=') ||
+        (input[i] === '>' && input[i + 1] === '='))) {
+        token = `${input[i]}${input[i + 1]}`;
+        tokenType = TOKEN_TYPES[token];
+        i++;
+        col_final = i - state.offset + 1;
+
+    } else {
+        token = input[i];
+        console.log(getTokenType(token))
+        tokenType = getTokenType(token);
+        col_final = i - state.offset + 1;
+
+        if (!isVocabulary(token)) {
+            error += `${token}  alfabeto-nao-identificado  ${state.line}  ${col_inicial}  ${col_final}<br>`
+        }
     }
-    
-    document.getElementById('token-output').innerHTML = output;
+
+    output += `${token}  ${tokenType}  ${state.line}  ${col_inicial}  ${col_final}<br>`;
+}
+
+document.getElementById('token-output').innerHTML = output;
+document.getElementById('error-output').innerHTML = error;
 }
